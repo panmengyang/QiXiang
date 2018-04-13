855015bbf35ca7a585298da96b105bcfcde236b9
unknown <350333678@qq.com>
2018-04-13 13:24:13 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/androidTest/java/com/example/qixiang/db/County.java
@@ -0,0 +1,42 @@
+package com.example.qixiang.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String countyCode) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
